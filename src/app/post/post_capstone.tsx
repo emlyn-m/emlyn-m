4149952c6a77d4e4d11343384c395b1dd8e30f85
Emlyn Matheson <emlyn@emlyn.xyz>
2025-12-03 01:53:51 +1100

    [feat] Add security section to Capstone post

--- src/app/post/post_capstone.tsx
+++ src/app/post/post_capstone.tsx
@@ -39,15 +39,15 @@ function FitImg(props: {src: string, className?: string, fitTo: RefObject<HTMLDi
 
 function RenderCapstonePost(): ReactElement {
 
-    const section1Ref = useRef(null);
-    const section2Ref = useRef(null);
-    const section3Ref = useRef(null);
+    const bgFeatRef = useRef(null);
+    const noticeboardRef = useRef(null);
+    const webRef = useRef(null);
 
     return (
         <div className="flex flex-col gap-5">
 
-            <div className="flex landscape:flex-row portrait:flex-col-reverse gap-5" ref={ section1Ref }>
-                <div className="flex flex-col gap-5 flex-1">
+            <div className="flex landscape:flex-row portrait:flex-col-reverse gap-5">
+                <div className="flex flex-col gap-5 flex-1" ref={ bgFeatRef }>
                     <div className="flex landscape:flex-row portrait:flex-col-reverse gap-5">
                         <div className="border-1 p-2 font-mono flex-1 flex flex-col gap-2" >
                             <h2 className="font-bold">Background</h2>
@@ -55,7 +55,7 @@ function RenderCapstonePost(): ReactElement {
                                 A requirement of my degree was the development of a non-trivial software project for an industry client.  For this, we chose to develop a control panel for TERRAINs vertical display screen (initially developed by <a href="https://trentcrawford.net" className="underline">Trent Cawford</a>).  This screen was initially designed to show the most recent videos uploaded to YouTube matching particular search parameters, however the  clients hoped that it could also be used as a general display screen for events and presentations.
                             </p>
                             <p className="text-s pl-4 pr-4 indent-4">
-                                As an additional project, we were asked to develop a digital Community Noticeboard for the display screen. This needed to be able to 
+                                As an additional project, we were asked to develop a digital Community Noticeboard for the display screen. This needed to be able to receive submissions from the general public and display them on the screen, with minimal intervention from TERRAIN staff.
                             </p>
                         </div>
                     </div>
@@ -64,30 +64,35 @@ function RenderCapstonePost(): ReactElement {
                         <h2 className="font-bold text-white">Features:</h2>
                         <ul className="p-1 text-white">
                             <li>• Community Noticeboard</li>
-                            <li>• Real-time control</li>
+                            <li>• Real-time control via SSE</li>
                             <li>• Automatic startup</li>
                             <li>• Automatic downloading/purging of YouTube videos</li>
+                            <li>• Access Control</li>
                         </ul>
                     </div>
 
                 </div>
-
-                <FitImg fitTo={section1Ref} src="/assets-capstone/screen-real.webp" className='w-min h-min border-1 p-3 box-border' />
+                <FitImg fitTo={ bgFeatRef } src="/assets-capstone/screen-real.webp" className='w-min h-min border-1 p-3 box-border' />
             </div>
 
             <div className="flex landscape:flex-row portrait:flex-col gap-5">
-                <FitImg fitTo={section2Ref} src="/assets-capstone/noticeboard.webp" className='w-min h-min border-1 p-3 box-border portrait:w-full min-h-[200px]' />
-                <div ref={ section2Ref } className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
+                <FitImg fitTo={ noticeboardRef } src="/assets-capstone/noticeboard.webp" className='w-min h-min border-1 p-3 box-border portrait:w-full min-h-[200px]' />
+                <div ref={ noticeboardRef } className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
                     <h2 className="font-bold">Community Noticeboard</h2>
                     <p className="text-s pl-4 pr-4 indent-4">
-                        body-nb-1
+                        Submission to the noticeboard was done via a simple Google Form. The posts themselves were hosted on a MongoDB Atlas instance, with a script running to periodically fetch results from the Google Form and insert them into the database. These posts would then go into a queue to be reviewed, and if they were approved, they would then be added to the screen. By default, posts will be archived after a week, however a custom one can be specified during the submission process.
                     </p>
+
+                    <p className="text-s pl-4 pr-4 indent-4">
+                        nb-2
+                    </p>
+
                 </div>
             </div>
 
 
             <div className="flex landscape:flex-row portrait:flex-col-reverse gap-5">
-                <div ref={ section3Ref } className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
+                <div ref={ webRef } className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
                     <h2 className="font-bold">External Websites</h2>
                     <p className="text-s pl-4 pr-4 indent-4">
                         While the display of uploaded files and YouTube videos was relatively simple, external websites were significantly more challenging. We needed to render them inside our website to allow for switching display modes, which meant using an iframe. Unfortunately, most websites send <Code>X-Frame-Options</Code> and <Code>CORS</Code> headers which restrict display in iframes (bummer!). 
@@ -96,8 +101,16 @@ function RenderCapstonePost(): ReactElement {
                         To work around these limitations, we developed a simple forward proxy to run locally. This proxy would receive the requested domain name as a subdomain on localhost (eg: google.com.localhost:3000/path), fetch it, and return the response (after stripping the relevant headers and rewriting any Set-Cookie headers). Supplying the target domain as a subdomain was necessary to ensure that relative paths worked correctly.
                     </p>
                 </div>
-                <FitImg fitTo={section3Ref} src="/assets-capstone/dashboard-full.webp" className='w-min h-min border-1 p-3 box-border' />
+                <FitImg fitTo={ webRef } src="/assets-capstone/dashboard-full.webp" className='w-min h-min border-1 p-3 box-border' />
             </div>
+
+            <div className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
+                <h2 className="font-bold">Security</h2>
+                <p className="text-s pl-4 pr-4 indent-4">
+                    As this screen is prominently displayed in store, avoiding unauthorized access was a key concern. The Raspberry Pi running the system was on a network that should remain unavailable to customers, but we wanted a second layer of defense. While setup for SSL (with DNS-backed certificates and an NGINX reverse proxy) was completed, this couldn't be deployed as we did not have access to DNS records at the time. This forced us to switch from OAuth/Google to HTTP basic authentication, however we still felt this was strong enough when combined with the network isolation.
+                </p>
+            </div>
+
 
         </div>
     )
