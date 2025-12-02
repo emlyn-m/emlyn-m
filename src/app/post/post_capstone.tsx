dc063b0a68ee911fa772a07bf308870d1201449f
Emlyn Matheson <emlyn@emlyn.xyz>
2025-12-03 00:19:28 +1100

    [fix] Use ResizeObserver for FitImg instead of just rerunning till convergance

--- src/app/post/post_capstone.tsx
+++ src/app/post/post_capstone.tsx
@@ -13,25 +13,26 @@ function FitImg(props: {src: string, className?: string, fitTo: RefObject<HTMLDi
 
     const imgRef = useRef(null);
 
-    function updateSize(retriggerCount: number) {
-        if (!props.fitTo.current) { return; }
-        if (!imgRef.current) { return; }
-        
-        const targetHeight = props.fitTo.current.clientHeight;        
-        
-        if (window.innerHeight > window.innerWidth) {
-            (imgRef.current as HTMLImageElement).style.maxHeight = `${targetHeight}px`;
-        } else {
-            (imgRef.current as HTMLImageElement).style.height = `${targetHeight}px`;
-        }
-        (imgRef.current as HTMLImageElement).style.display = 'block';
-
-        if (retriggerCount) { updateSize(retriggerCount-1); }
-    }
 
     useEffect(() => {
-        updateSize(3);
-    }, [ props.fitTo ])
+        if (!props.fitTo.current) return;
+
+        const resizeObserver = new ResizeObserver(() => {
+            if (!imgRef.current) { return; }
+        
+            const targetHeight = (props.fitTo.current as HTMLImageElement).clientHeight;        
+            
+            if (window.innerHeight > window.innerWidth) {
+                (imgRef.current as HTMLImageElement).style.maxHeight = `${targetHeight}px`;
+            } else {
+                (imgRef.current as HTMLImageElement).style.height = `${targetHeight}px`;
+            }
+            (imgRef.current as HTMLImageElement).style.display = 'block';
+        });
+        resizeObserver.observe(props.fitTo.current);
+        return () => resizeObserver.disconnect();
+
+    }, [ props.fitTo, props.fitTo.current?.clientHeight ]);
 
     return (
         <img ref={imgRef} className={props.className} src={ props.src } style={{ display: 'none' }}/>
@@ -42,6 +43,7 @@ function RenderCapstonePost(): ReactElement {
 
     const section1Ref = useRef(null);
     const section2Ref = useRef(null);
+    const section3Ref = useRef(null);
 
     return (
         <div className="flex flex-col gap-5">
@@ -60,36 +62,35 @@ function RenderCapstonePost(): ReactElement {
                         </div>
                     </div>
 
-                    <div className="flex landscape:flex-row portrait:flex-col gap-5">
-                        <div className="border-1 p-2 font-mono landscape:wrap-none mobile:wrap bg-black">
-                            <h2 className="font-bold text-white">Features:</h2>
-                            <ul className="p-1 text-white">
-                                <li>• Automatic downloading of YouTube videos</li>
-                                <li>• f2</li>
-                                <li>• f3</li>
-                                <li>• f4</li>
-                                <li>• f5</li>
-
-                            </ul>
-                        </div>
-
-                        <div className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
-                            <h2 className="font-bold">Synchronisation</h2>
-                            <p className="text-s pl-4 pr-4 indent-4">
-                                body-sync-1
-                            </p>
-                        </div>
-
+                    <div className="border-1 p-2 font-mono landscape:wrap-none mobile:wrap bg-black">
+                        <h2 className="font-bold text-white">Features:</h2>
+                        <ul className="p-1 text-white">
+                            <li>• Community Noticeboard</li>
+                            <li>• Real-time control</li>
+                            <li>• Automatic startup</li>
+                            <li>• Automatic downloading/purging of YouTube videos</li>
+                            <li>• f5</li>
+                        </ul>
                     </div>
 
                 </div>
 
-                <FitImg fitTo={section1Ref} src="/assets-capstone/screen-1.webp" className='w-min h-min border-1 p-3 box-border' />
+                <FitImg fitTo={section1Ref} src="/assets-capstone/screen-real.webp" className='w-min h-min border-1 p-3 box-border' />
             </div>
 
             <div className="flex landscape:flex-row portrait:flex-col gap-5">
-                <FitImg fitTo={section2Ref} src="/assets-capstone/screen-1.webp" className='w-min h-min border-1 p-3 box-border' />
+                <FitImg fitTo={section2Ref} src="/assets-capstone/dashboard-full.webp" className='w-min h-min border-1 p-3 box-border' />
                 <div ref={ section2Ref } className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
+                    <h2 className="font-bold">Community Noticeboard</h2>
+                    <p className="text-s pl-4 pr-4 indent-4">
+                        body-nb-1
+                    </p>
+                </div>
+            </div>
+
+
+            <div className="flex landscape:flex-row portrait:flex-col gap-5">
+                <div ref={ section3Ref } className="border-1 p-2 font-mono flex-1 flex flex-col gap-2">
                     <h2 className="font-bold">External Websites</h2>
                     <p className="text-s pl-4 pr-4 indent-4">
                         While the display of uploaded files and YouTube videos was relatively simple, external websites were significantly more challenging. We needed to render them inside our website to allow for switching display modes, which meant using an iframe. Unfortunately, most websites send <Code>X-Frame-Options</Code> and <Code>CORS</Code> headers which restrict display in iframes (bummer!). 
@@ -98,7 +99,7 @@ function RenderCapstonePost(): ReactElement {
                         To work around these limitations, we developed a simple forward proxy to run locally. This proxy would receive the requested domain name as a subdomain on localhost (eg: google.com.localhost:3000/path), fetch it, and return the response (after stripping the relevant headers and rewriting any Set-Cookie headers). Supplying the target domain as a subdomain was necessary to ensure that relative paths worked correctly.
                     </p>
                 </div>
-
+                <FitImg fitTo={section3Ref} src="/assets-capstone/dashboard-full.webp" className='w-min h-min border-1 p-3 box-border' />
             </div>
 
         </div>
