83fe8bebc19f6817340ffd99e26df283dd445e2c
Emlyn Matheson <emlyn@emlyn.xyz>
2025-12-02 20:54:55 +1100

    [feat] Add post_header component

---
+++ src/app/components/post_header.tsx
@@ -0,0 +1,19 @@
+import Button from "./button";
+
+export interface PostHeaderProps {
+    name: string,
+}
+
+export default function PostHeader(props: PostHeaderProps) {
+    return (
+        <div className="pl-4 pb-2 pt-0 border-b-1 flex flex-row items-end gap-2 ">
+            <Button className="w-[2rem] h-[2rem] hover:invert hover:bg-white hover:border-white" href="/">
+                <img src='/logo-transparent.svg' />
+            </Button>
+            <div className="w-[1px] h-[2rem] bg-black"></div>
+            <h1 className='text-[1.5rem] leading-[1.333] font-bold font-mono'>{ props.name }</h1>
+
+            <span className="flex-1 text-end font-mono font-light text-xs cursor-default select-none">© 2025</span>
+        </div>
+    )
+}
